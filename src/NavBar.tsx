291ced753d58847a15939af831b24445a23f73b9
Timon Sachweh <timon@sachweh.de>
2024-10-30 16:27:06 +0100

    Add coloring to navbar and setting background-color for page

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,15 +1,28 @@
 import {NavLink} from "react-router-dom";
+import tuLogo from './assets/tud_logo.svg';
+import romsLogo from './assets/roms_logo.png';
 
 function NavBar() {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/routing">Routing</NavLink></li>
-        <li><NavLink to="/gaiax">GaiaXAssets</NavLink></li>
-      </ul>
-    </nav>
-  );
+    const baseNav = "font-bold hover:bg-tuGreen text-white py-2 px-4 rounded transition duration-200 hover:ease-in-out motion-reduce:transition-none";
+    const activeNav = baseNav + " bg-tuGreen";
+    const inactiveNav = baseNav + " bg-gaiaPurple";
+
+    return (
+        <nav
+            className="bg-slate-100 flex-nowrap relative flex w-full items-center justify-between py-2 lg:flex-wrap lg:py-4 space-x-20">
+            <div className="grow-0 pl-2">
+                <img src={tuLogo} alt="TU Logo" className="h-8"/>
+            </div>
+            <div className="grow-1 space-x-10 mt-1 mb-1 justify-center">
+                <NavLink to="/" className={({isActive}) => (isActive ? activeNav : inactiveNav)}>Home</NavLink>
+                <NavLink to="/routing" className={({isActive}) => (isActive ? activeNav : inactiveNav)}>Routing</NavLink>
+                <NavLink to="/gaiax" className={({isActive}) => (isActive ? activeNav : inactiveNav)}>Gaiax</NavLink>
+            </div>
+            <div className="grow-0 pr-2">
+                <img src={romsLogo} alt="TU Logo" className="h-8"/>
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gaiaPurple: '#B908FF',
+        tuGreen: '#7AC142',
+      },
+    }
   },
   plugins: [],
 }
